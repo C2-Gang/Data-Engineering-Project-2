c819d14c10a016aabdb56a5cb6df27b0e744239e
Chloe CARAYON <chloe.carayon@efrei.net>
2022-03-04 13:09:08 +0100

    feat: add prometheus with flask wip #DEP2-9

--- back-end/app.py
+++ back-end/app.py
@@ -3,14 +3,35 @@ from flask import Flask, render_template, request, jsonify, g
 import json
 from flask_cors import CORS, cross_origin
 from detoxify import Detoxify
+from prometheus_flask_exporter import PrometheusMetrics
+import random
+import time
 
 app = Flask(__name__)
 CORS(app,  support_credentials=True)
 
 MODEL = Detoxify('original')
 
+metrics = PrometheusMetrics(app)
+endpoints = ("toxic")
+
+by_path_counter = metrics.counter(
+    'by_path_counter', 'Request count by request paths',
+    labels={'path': lambda: request.path}
+)
+
+common_counter = metrics.counter(
+    'by_endpoint_counter', 'Request count by endpoints',
+    labels={'endpoint': lambda: request.endpoint}
+)
+
+summary = metrics.summary('requests_by_status', 'Request latencies by status',
+                 labels={'status': lambda r: r.status_code})
+histogram = metrics.histogram('requests_by_status_and_path', 'Request latencies by status and path',
+                   labels={'status': lambda r: r.status_code, 'path': lambda: request.path})
+
 @app.route('/')
-def index():
+def base():
     output = {'output': 'this is an expected output'}
     return jsonify(output)
 
@@ -40,6 +61,11 @@ def predict_text(text: str):
 
 @app.route('/toxic',methods=['GET'])
 @cross_origin(supports_credentials=True)
+@metrics.gauge('in_progress', 'Long running requests in progress')
+@by_path_counter
+@common_counter
+@summary
+@histogram
 def toxic():
     """This function retrieve the text from the user, apply on it the prediction and return the toxicity.
     :return: jsonify(toxicity)
